8e93e9155a849120738098fd4c717d25a1c28c05
Eliseo Henriquez <rufoslk@gmail.com>
2016-06-13 00:14:31 -0300

    testing new constructor from Profesor.java in main, some errors

--- src/ejerciciojavaherenciaimplements/EjercicioJavaHerenciaImplements.java
+++ src/ejerciciojavaherenciaimplements/EjercicioJavaHerenciaImplements.java
@@ -2,6 +2,11 @@ package ejerciciojavaherenciaimplements;
 
 public class EjercicioJavaHerenciaImplements {
     public static void main(String[] args) {
+        Proyecto pro = new Proyecto("educar");
+        Profesor p = new Profesor("fijo", pro,"18946948-9","Juan",true);
+        p.rutNombre();
+        p.ImprimirStatus();
+        p.toString();
     }
     
 }
